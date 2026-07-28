41c5f8d3885dad2b09148a3dcfc53734b8da865a
pipapo888 <kazuya2002uk@gmail.com>
2026-07-28 19:51:05 +0900

    main　　追加

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 16:16:52 by knomura           #+#    #+#             */
-/*   Updated: 2026/07/28 18:59:46 by knomura          ###   ########.fr       */
+/*   Updated: 2026/07/28 19:50:53 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@ int main()
 
 	p2.attack("pen");
 	p2.guardGate();
+	p2.takeDamage(60);
+
+	ScavTrap p3 = p2;
+	p3.beRepaired(5);
+	p3.takeDamage(60);
+	p3.attack("p2");
 	
 	std::cout << "------------Main END------------\n";
 
